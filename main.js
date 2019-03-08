3db438c8c969f8ebdf251db7c1a278c9cfabae04
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-08 17:08:16 -0500

    created basic class set up

---
+++ main.js
@@ -0,0 +1,28 @@
+class UI {
+    constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+    }
+}
+
+function eventListeners () {
+    const budgetForm = document.getElementById('budget-form');
+    const expenseForm = document.getElementById('expense-form');
+    const expenseList = document.getElementById('expense-list');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+})
