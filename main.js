87444dcae580ccb132c707990bc816618fcd747a
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-08 17:51:57 -0500

    Added showBalance method, totalExpense method, and submitBudgetForm methods to class UI

--- main.js
+++ main.js
@@ -15,12 +15,69 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
     }
+    // submit budget method
+    submitBudgetForm() {
+        const value = this.budgetInput.value;
+        if (value === '' || value < 0) {
+            this.budgetFeedback.classList.add('showItem');
+            this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+            const self = this;
+            //console.log(this);
+            
+            setTimeout(function () {
+                self.budgetFeedback.classList.remove('showItem');
+            }, 3000);
+        } else {
+            this.budgetAmount.textContent = value;
+            this.budgetInput.value = '';
+            this.showBalance();
+        }
+    }
+    // show balance 
+    showBalance () {
+        const expense = this.totalExpense();
+        const total = parseInt(this.budgetAmount.textContent) - expense;
+        this.balanceAmount.textContent = total;
+        if (total < 0) {
+            this.balance.classList.remove('showGreen', 'showBlack');
+            this.balance.classList.add('showRed');
+        } else if (total > 0) {
+            this.balance.classList.remove('showRed', 'showBlack');
+            this.balance.classList.add('showGreen');
+        } else if (total === 0) {
+            this.balance.classList.remove('showRed', 'showGreen');
+            this.balance.classList.add('showBlack');
+        }
+    }
+    // total expense
+    totalExpense() {
+        let total = 400;
+        return total;
+    }
 }
 
 function eventListeners () {
     const budgetForm = document.getElementById('budget-form');
     const expenseForm = document.getElementById('expense-form');
     const expenseList = document.getElementById('expense-list');
+
+    // new instance of UI class
+    const ui = new UI (); 
+
+    // budget form submit
+    budgetForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        ui.submitBudgetForm();
+    })
+
+    expenseForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+    })
+
+    expenseList.addEventListener('click', function (event) {
+        event.preventDefault();
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
