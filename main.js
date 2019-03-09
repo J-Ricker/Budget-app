97e2669558aa6578a64a3962b0482ef7af53b345
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-09 12:02:56 -0500

    Adding expense class div to index.html with javascript

--- main.js
+++ main.js
@@ -80,6 +80,20 @@ class UI {
     addExpense (expense) {
         const div = document.createElement('div');
         div.classList.add('expense');
+        div.innerHTML=`
+            <div class="expense-item d-flex justify-content-between align-items-baseline">
+                    <h6 class="expense-title mb-0 text-uppercase list-item">- title</h6>
+                    <h5 class="expense-amount mb-0 list-item">amount</h5>
+                    <div class="expense-icons list-item">
+                    <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+                    <i class="fas fa-edit"></i>
+                    </a>
+                    <a href="#" class="delete-icon" data-id="${expense.id}">
+                    <i class="fas fa-trash"></i>
+                    </a>
+                    </div>
+            </div>
+        `;
     }
 
     // total expense
